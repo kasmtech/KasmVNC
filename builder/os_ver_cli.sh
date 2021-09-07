91eb953f627c4a4532bfdd52198865d145c22c83
Dmitry Maksyoma <ledestin@gmail.com>
2021-09-08 00:11:21 +1200

    Feature/kasm 1838 libjpeg turbo from source (#49)
    New build option with latest stable libjpeg-turbo library

--- builder/os_ver_cli.sh
+++ builder/os_ver_cli.sh
@@ -3,6 +3,11 @@ default_os_codename=bionic
 
 os=${1:-$default_os}
 os_codename=${2:-$default_os_codename}
+build_tag="${3:-}"
+if [[ -n "$build_tag" ]]; then
+   build_tag_for_images="_${build_tag#+}"
+   build_debian_revision="$(echo $build_tag | tr _ -)"
+fi
 os_image="$os:$os_codename"
 
 echo "Building for $os_image"
