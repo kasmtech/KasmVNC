75b42e4f3f48e18d8b8c0dd91ea9566e59b39f91
Dmitry Maksyoma <ledestin@gmail.com>
2021-09-09 00:25:21 +1200

    select-de.sh: assume -y if -s <DE> was given

--- builder/startup/deb/select-de.sh
+++ builder/startup/deb/select-de.sh
@@ -35,6 +35,7 @@ process_cli_options() {
         action=select-de
         if [[ -n "$1" && "${1:1}" != "-" ]]; then
           selected_de="$1"
+          assume_yes=1
           if [ "$selected_de" = "manual" ]; then
             selected_de="$manual_xstartup_choice"
           fi
