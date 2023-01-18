23676ab3b3928e5e7bde3162e24fd58369d7a25e
Dmitry Maksyoma <ledestin@gmail.com>
2023-01-19 01:35:36 +1300

    Support newest libjpeg-turbo on Centos

---
+++ builder/scripts/common.sh
@@ -0,0 +1,13 @@
+detect_distro() {
+  if [ -f /etc/centos-release ]; then
+    DISTRO=centos
+  elif [ -f /etc/oracle-release ]; then
+    DISTRO=oracle7
+  elif [ -f /usr/bin/zypper ]; then
+    DISTRO=opensuse
+  else
+    DISTRO=debian
+  fi
+}
+
+detect_distro
