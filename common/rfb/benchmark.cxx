9e79d8ae56b039ca098125448b19f383ba96af9c
El <eldargafiyatov@gmail.com>
2025-04-21 17:17:06 +0500

    KASM-6984 Update copyright information in benchmark files

--- common/rfb/benchmark.cxx
+++ common/rfb/benchmark.cxx
@@ -1,5 +1,5 @@
-/* Copyright (C) 2025 Kasm Web
-*
+/* Copyright (C) 2025 Kasm Technologies Corp
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
