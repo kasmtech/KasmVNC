0244c47d7a462c713ebdf35d40b303a828735676
Dmitry Maksyoma <ledestin@gmail.com>
2025-05-10 09:52:34 +0000

    KASM-7052 Add test-barebones

--- .ci/upload.sh
+++ .ci/upload.sh
@@ -61,24 +61,80 @@ function prepare_upload_filename() {
   fi
 };
 
+list_files_in_directory() {
+  local dir="$1"
+  find "$1" -mindepth 1
+}
+
+upload_directory_to_s3() {
+  local dir_to_upload="$1"
+  local s3_directory="$2";
+  local s3_bucket="$3";
+
+  for file_to_upload in $(list_files_in_directory "$dir_to_upload"); do
+    upload_to_s3 "$file_to_upload" "$s3_directory/$file_to_upload" "$s3_bucket"
+  done
+}
+
+prepare_functional_tests_source_and_cd_into_it() {
+  git clone https://gitlab-ci-token:$CI_JOB_TOKEN@gitlab.com/kasm-technologies/internal/kasmvnc-functional-tests.git
+  cd kasmvnc-functional-tests
+  mkdir output && chown 1000:1000 output
+  mkdir report && chown 1000:1000 report
+}
+
+upload_report_to_s3() {
+  s3_tests_directory="kasmvnc/${CI_COMMIT_SHA}/tests"
+  upload_directory_to_s3 report "$s3_tests_directory" "$S3_BUCKET"
+  aws s3 cp report/index.html "s3://${S3_BUCKET}/${s3_tests_directory}/report/index.html" --metadata-directive REPLACE --content-type "text/html"
+}
+
+put_report_into_ci_pipeline() {
+  report_name="Functional%20test%20report"
+  report_url="https://${S3_BUCKET}.s3.amazonaws.com/${s3_tests_directory}/report/index.html"
+  curl --request POST --header "PRIVATE-TOKEN:${GITLAB_API_TOKEN}" "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/statuses/${CI_COMMIT_SHA}?state=success&name=${report_name}&target_url=${report_url}"
+}
+
+prepare_kasmvnc_built_packages_to_replace_workspaces_image_packages() {
+  cp -r ../output/jammy output/
+}
+
+prepare_to_run_functional_tests() {
+  install_packages_needed_for_functional_tests
+  prepare_functional_tests_source_and_cd_into_it
+  prepare_s3_uploader
+  prepare_kasmvnc_built_packages_to_replace_workspaces_image_packages
+}
+
+install_packages_needed_for_functional_tests() {
+  export DEBIAN_FRONTEND=noninteractive
+  apt-get update && apt-get install -y git tree curl docker.io awscli
+  apt-get install -y ruby3.1 wget
+  apt-get install -y python3 python3-pip python3-boto3 curl pkg-config libxmlsec1-dev
+}
+
 function upload_to_s3() {
-  local package="$1";
-  local upload_filename="$2";
+  local file_to_upload="$1";
+  local s3_url_for_file="$2";
   local s3_bucket="$3";
 
   # Transfer to S3
-  python3 amazon-s3-bitbucket-pipelines-python/s3_upload.py "${s3_bucket}" "$package" "${upload_filename}";
+  python3 amazon-s3-bitbucket-pipelines-python/s3_upload.py "$s3_bucket" "$file_to_upload" "$s3_url_for_file";
   # Use the Gitlab API to tell Gitlab where the artifact was stored
-  export S3_URL="https://${s3_bucket}.s3.amazonaws.com/${upload_filename}";
+  export S3_URL="https://${s3_bucket}.s3.amazonaws.com/${s3_url_for_file}";
 };
 
+function prepare_s3_uploader() {
+  git clone https://bitbucket.org/awslabs/amazon-s3-bitbucket-pipelines-python.git
+}
+
 function prepare_to_run_scripts_and_s3_uploads() {
-  export DEBIAN_FRONTEND=noninteractive;
-  apt-get update;
-  apt-get install -y ruby2.7 git wget;
-  apt-get install -y python3 python3-pip python3-boto3 curl pkg-config libxmlsec1-dev;
-  git clone https://bitbucket.org/awslabs/amazon-s3-bitbucket-pipelines-python.git;
-};
+  export DEBIAN_FRONTEND=noninteractive
+  apt-get update
+  apt-get install -y ruby2.7 git wget
+  apt-get install -y python3 python3-pip python3-boto3 curl pkg-config libxmlsec1-dev
+  prepare_s3_uploader
+}
 
 detect_release_branch() {
   if echo $CI_COMMIT_REF_NAME | grep -Pq '^release/([\d.]+)$'; then
